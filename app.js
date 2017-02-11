13772c8b83cfb93d3d89196c6bdb482b0d35a932
Ken Lee <kzl5010@gmail.com>
2017-02-11 14:24:39 -0800

    finished project

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
+const path = require('path');
 var app = require('express')();
+
+var fs = require('fs');
+
+app.post('/events', function(request, response){
+  response.send("GET");
+});
+
+// app.use(function (req, res, next) {
+//   let err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+
+// catch 404 and forward to error handler
 
 var listener = app.listen(8080, function(){
     console.log('Listening on port ' + listener.address().port); //Listening on port 8888
